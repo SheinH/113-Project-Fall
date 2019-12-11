7522b2c1626302afb92f24e09fc9d814e616b926
nicksweatherley <nick.s.weatherley@protonmail.com>
2019-12-11 09:46:56 -0500

    Added file line count. Added user friendliness to classification loop.

--- letter_classifier.py
+++ letter_classifier.py
@@ -4,23 +4,13 @@ import os
 
 from data_generator import HGenerator, LGenerator
 
+
 class LetterClassifier():
     ''' Class is used to show and classify data '''
     cwd = os.getcwd()
-    training_h_file_name = cwd + '/H Training.txt'
-    training_l_file_name = cwd + '/L Training.txt'
+    training_h_file_name = cwd + '/Data/H Training.txt'
+    training_l_file_name = cwd + '/Data/L Training.txt'
 
-    def __show_letter(self, letter):
-        '''Shows letter as pixels of zeros and ones
-
-        Keyword arguments:
-        letter -- array length 12 of 0s and 1s
-        '''
-        for pos in range(len(letter)):
-            print(letter[pos], end = ' ')
-            if ((pos + 1) % 3 == 0):
-                print()
-    
     def __classify(self, letter):
         ''' Classify the letter shown as either H or L and return choice as CSV.
 
@@ -33,8 +23,36 @@ class LetterClassifier():
         if(classifification == ''):
             return '', ''
         else:
-            return ','.join([str(letter).strip('[]').replace(' ', ''),classifification.upper()]), classifification.upper()
-    
+            return ','.join([str(letter).strip('[]').replace(' ', ''), classifification.upper()]), classifification.upper()
+
+    def __count_file_lines(self, filename):
+        ''' Count the lines of a file if it exists
+
+        Keyword arguments:
+        filename -- string representation of where file is located on the OS
+        '''
+        count = 0
+        try:
+            f = open(filename, 'r')
+        except:
+            return 0
+
+        for l in f:
+            count += 1
+
+        return count
+
+    def __show_letter(self, letter):
+        '''Shows letter as pixels of zeros and ones
+
+        Keyword arguments:
+        letter -- array length 12 of 0s and 1s
+        '''
+        for pos in range(len(letter)):
+            print(letter[pos], end=' ')
+            if ((pos + 1) % 3 == 0):
+                print()
+
     def __write_data_to_files(self, h_set, l_set):
         ''' Append training data to respective files.
 
@@ -49,34 +67,49 @@ class LetterClassifier():
         f = open(self.training_l_file_name, 'a+')
         f.write('\n'.join(l_set))
         f.close()
-    
+
     def classifyLetters(self):
         ''' Classify a minimium of 300 numbers for the L set and H set.'''
         h_gen = HGenerator()
         l_gen = LGenerator()
-        classified_letters = []
+        h_done = self.__count_file_lines(self.training_h_file_name)
+        l_done = self.__count_file_lines(self.training_l_file_name)
 
+        classified_letters = []
         h_set = []
         l_set = []
+        iteration = 0
 
-        while len(h_set) < 300 or len(l_set) < 300:
-            if(len(h_set) >= 300):
+        while l_done < 300 or h_done < 300:
+            print('{} out of {} Ls completed'.format(l_done, 300))
+            print('{} out of {} Hs completed'.format(h_done, 300))
+
+            if(l_done >= 300):
                 letter = l_gen.getNewLetterWithCorrupt()
-            elif(len(l_set) >= 300):
+            elif(h_done >= 300):
                 letter = h_gen.getNewLetterWithCorrupt()
             else:
-                if(random.randint(1,2) == 1):
+                if(random.randint(1, 2) == 1):
                     letter = l_gen.getNewLetterWithCorrupt()
                 else:
                     letter = h_gen.getNewLetterWithCorrupt()
-            
+
             c, v = self.__classify(letter)
             if(c != ''):
                 if(v == 'H'):
                     h_set.append(c)
+                    h_done += 1
+                    iteration += 1
                 elif(v == 'L'):
                     l_set.append(c)
-        self.__write_data_to_files(h_set, l_set)
-    
-            
+                    h_done += 1
+                    iteration += 1
 
+            # Save after 20 new data points as do not want to lose data on crash or boredom
+            if(iteration >= 20):
+                iteration = 0
+                self.__write_data_to_files(h_set, l_set)
+                h_set = []
+                l_set = []
+
+        self.__write_data_to_files(h_set, l_set)
